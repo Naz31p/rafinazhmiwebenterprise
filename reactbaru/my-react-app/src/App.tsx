1579181a14e35982d08b706111f1a14cd21233f8
Naz31P <rafinazhmi924@gmail.com>
2025-04-25 06:49:58 +0700

    Web Enterprise Materi React Dasar

---
+++ reactbaru/my-react-app/src/App.tsx
@@ -0,0 +1,11 @@
+import { UserCard } from './UserCard'
+function App() {
+  return (
+    <div>
+      <UserCard name="Rafi Nazhmi" job="Web Developer" avatarUrl="http://localhost:5173/vite.svg" />
+      <UserCard name="John Doe" job="UI Designer" avatarUrl="http://localhost:5173/vite.svg" />
+    </div>
+  );
+}
+
+export default App;
---
+++ reactbaru/my-react-app/src/HelloWorld.tsx
@@ -0,0 +1,9 @@
+function HelloWorld() {
+    return (
+      <>
+        <h1>Hello Everyone!</h1>
+      </>
+    )
+  }
+  
+  export default HelloWorld
